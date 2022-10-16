2623d494dbd4e717dc2efc4ea30eedc8dfe3eb3f
Ana T <44075998+adteruya@users.noreply.github.com>
2022-10-15 22:03:41 -0700

    Reorganized screens + new screen additions
    Created a separate login screen file and implemented landscape orientation for login screen. Completed the portrait mode create account screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,90 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         fontFamily: 'Lato',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Icon(
-              Icons.school,
-              size: 130.0
-            ),
-            // Title pending...
-            const Text(
-              'Language Learning App',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w700
-              ),
-            ),
-            const SizedBox(height: 5),
-            const Text(
-              'a crowd-sourced language learning application',
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w300
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                  decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Please enter your email address'
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                  decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Please enter your password'
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.black
-              ),
-              // TODO: Create Selection Screen
-              onPressed: () { }, 
-              child: const Text('Login')
-            ),
-            TextButton(
-              // TODO: Create Registration Screen
-              onPressed: () { }, 
-              child: const Text('Sign Up')
-            )
-          ],
-        ),
-      )
+      home: const HomeLoginScreen(title: 'Learning App Login'),
     );
   }
 }
---
+++ lib/screens/home_login_screen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'create_account_screen.dart';
+
+class HomeLoginScreen extends StatefulWidget {
+  const HomeLoginScreen({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomeLoginScreen> createState() => _HomeLoginScreenState();
+}
+
+class _HomeLoginScreenState extends State<HomeLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait)  {
+            return _portraitModeLogin();
+          } else  {
+            return _landscapeModeLogin();
+          }
+        }
+      )
+    );
+  }
+
+  Widget _portraitModeLogin() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Icon(
+            Icons.school,
+            size: 130.0
+          ),
+          // Title pending...
+          const Text(
+            'Language Learning App',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'a crowd-sourced language learning application',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w300
+            ),
+          ),
+          const SizedBox(height: 30),
+          // TODO: Will be updated to forms later on
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: TextField(
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                hintText: 'Please enter your email address'
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: TextField(
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Please enter your password'
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.black
+            ),
+            // TODO: Create Selection Screen
+            onPressed: () { }, 
+            child: const Text('Login')
+          ),
+          TextButton(
+            // TODO: Create Registration Screen
+            onPressed: () { 
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: ((context) => CreateAccountScreen())), 
+              );
+            }, 
+            child: const Text('Sign Up')
+          )
+        ],
+      ),
+    );
+  }
+  
+  Widget _landscapeModeLogin()  {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.school,
+                  size: 100.0
+                ),
+                // Title pending...
+                const Text(
+                  'Language Learning App',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const Text(
+                  'a crowd-sourced language learning application',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300
+                  ),
+                ),
+              ]
+            )
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // TODO: Will be updated to forms later on
+                const Padding( 
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                      decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      hintText: 'Please enter your email address'
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Padding( 
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                      decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Please enter your password'
+                    ),
+                  )
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [                
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black
+                      ),
+                      // TODO: Create Selection Screen
+                      onPressed: () { }, 
+                      child: const Text('Login')
+                    ),
+                    TextButton(
+                      // TODO: Create Registration Screen
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: ((context) => CreateAccountScreen())), 
+                          );
+                        }, 
+                      child: const Text('Sign Up')
+                    )
+                  ]
+                ),
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
+}
