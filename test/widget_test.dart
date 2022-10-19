444db60e4c0e22e1f2b527cb8f1837e20eb61f4b
Nathanael <nathanaellmartinez@gmail.com>
2022-10-19 18:19:55 -0500

    add firebase dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'package:cs467_language_learning_app/main.dart';
 import 'package:cs467_language_learning_app/screens/create_account_screen.dart';
 import 'package:cs467_language_learning_app/screens/home_login_screen.dart';
-
-import 'package:mocktail/mocktail.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
